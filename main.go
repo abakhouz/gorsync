28e91978ad48feafbc360a8679116cb723052285
Tom de Bruijn <tom@tomdebruijn.com>
2014-12-29 22:28:21 +0100

    Execute a command from go

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
+import "fmt"
+import "os"
+import "os/exec"
+import "path"
+
+const utility = "rsync"
+
 func main() {
+	options := []string{
+		"-a",
+		path.Join(getCurrentDirectory(), "a"),
+		path.Join(getCurrentDirectory(), "b"),
+	}
+	sync(options)
+}
+
+func sync(options []string) {
+	out, error := exec.Command(utility, options...).Output()
+	if error != nil {
+		fmt.Println("An error occurred while syncing!")
+		fmt.Println("%s", error)
+	}
+	fmt.Printf("%s", out)
+}
+
+func getCurrentDirectory() string {
+	pwd, error := os.Getwd()
+	if error != nil {
+		fmt.Println(error)
+		os.Exit(1)
+	}
+	return pwd
 }
